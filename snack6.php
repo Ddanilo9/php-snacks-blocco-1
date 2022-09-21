ff314fcbaa3f7e9e21e4c2722a2b34ac09360a82
Ddanilo9 <interiordanilo3@gmail.com>
2022-09-21 15:49:44 +0200

    snack5

---
+++ snack6.php
@@ -0,0 +1,49 @@
+<?php
+
+$db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+ 
+?>
+
+<!-- <ul>
+    <?php
+    for ($i = 0; $i < count($partite); $i++) {
+    ?>
+        <li>
+            <?php
+            echo "{$partite[$i]['home']} - {$partite[$i]['away']} | "
+            ?>
+            <strong>
+                <?php
+                echo "{$partite[$i]['homeScore']} - {$partite[$i]['awayScore']}"
+                ?>
+            </strong>
+        </li>
+    <?php
+    }
+    ?>
+</ul> -->
+
+<div>
+    
+</div>
